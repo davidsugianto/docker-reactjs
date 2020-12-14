6717b9c9f3040ada987e6dc8a7984d23e92ac807
davidmufc98 <davidmufc98@gmail.com>
2020-12-14 10:43:26 +0700

    add test app

---
+++ public-html/react-test/precache-manifest.4bbebb2af7f69f6df40cd61898636a8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325a08d9626fc46d5c8718c3658e6470",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01a0cd70985759bd52d6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11433a4a6c84f0b9df1e",
+    "url": "/static/js/2.7ffb8a59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ffb8a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a0cd70985759bd52d6",
+    "url": "/static/js/main.d2104f3f.chunk.js"
+  },
+  {
+    "revision": "279a83962c9cd2d631f1",
+    "url": "/static/js/runtime-main.fb15dc4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
